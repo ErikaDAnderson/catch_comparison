1ff2b32314c1b1fa3840f210fc41229b6d22ee52
ErikaDAnderson <Erika.Anderson@dfo-mpo.gc.ca>
2026-06-25 17:04:32 -0700

    preliminary graphing and QC on data

--- rscripts/catch_comparison.R
+++ rscripts/catch_comparison.R
@@ -8,10 +8,13 @@ library(DBI)
 library(ROracle)
 library(getPass)
 library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
 
 #define parameters used in scripts
 start_date <- "04/01/2020 00:00"
-end_date <- "03/31/2021 23:59"
+end_date <- "03/31/2025 23:59"
 ATIP_SAFE <- "N"
 pfma_list <- "23,101"
 fishery_list <- "5,6,7"
@@ -64,11 +67,38 @@ fisher_df <- query_df(oracle_con, fisher_reported_sql,
 # close connection
 DBI::dbDisconnect(oracle_con)
 
-
-# fisher reported cacth estimates summed by area and year
-
 # graph both types of catch estimates by year
-# seperate areas in different panels
+# separate areas in different panels
+# combine datasets
+combined_df <- bind_rows(in_season_df, fisher_df) %>%
+    rename(
+    pfma = MGMT_AREA,
+    year = CALENDAR_YEAR,
+    source= ESTIMATE_TYPE
+  ) %>%
+    mutate(
+    pfma = factor(pfma),
+    year = as.numeric(year),
+    source = factor(source)
+  )
+
+
+ggplot(combined_df,
+       aes(x = year, y = SOCKEYE_KEPT, fill = source)) +
+  geom_col(position = position_dodge(width = 0.8)) +
+  facet_wrap(~pfma)+
+  labs(
+    title = "Annual Catch Comparison by Source",
+    x = "Year",
+    y = "Count",
+    fill = "Estimate Source"
+  ) +
+  theme_minimal() +
+  theme(
+    strip.text = element_text(face = "bold"),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
 
 # determine greatest differences between catch estimates
 # from same year and area
